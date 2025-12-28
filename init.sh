28b62b2c3aaa88bd03edb2624c0f7b5c46012cf2
hannedouche.alex <hannedouche.alex@gmail.com>
2025-12-28 10:58:50 +0100

    changeg

--- init.sh
+++ init.sh
@@ -1,5 +1,9 @@
 #! /bin/sh
 
-ln -s ~/.dotfiles/config/helix ~/.config/
-ln -s ~/.dotfiles/config/niri ~/.config/
-ln -s ~/.dotfiles/config/alacritty ~/.config/
+rm -r ~/.config/helix && ln -s ~/.dotfiles/config/helix ~/.config/
+rm -r ~/.config/niri && ln -s ~/.dotfiles/config/niri ~/.config/
+rm -r ~/.config/alacritty && ln -s ~/.dotfiles/config/alacritty ~/.config/
+rm -r ~/.config/waybar && ln -s ~/.dotfiles/config/waybar ~/.config
+rm -r ~/.config/nushell && ln -s ~/.dotfiles/config/nushell ~/.config
+
+swww img ~/.dotfiles/wallpaper/wallpaper.jpg
